679b55c3336c3626e21168037f70bae34eea27ac
skrphenix <76821526+skrphenix@users.noreply.github.com>
2021-11-30 21:17:51 +0530

    0.1.2

--- Examples/buttons.py
+++ Examples/buttons.py
@@ -1,32 +1,4 @@
 """
-Simple Response Button
-"""
-
-btn = SButton(label="Hello", response="Hello, Have a nice day !")
-view_ = Button(author, btn).view()
-
-"""
-2 Simple Responsive Buttons 
-"""
-
-btn1 = SButton(label="Hello", response="Hello, Have a nice day !")
-btn2 = SButton(label="Bye", response="Bye, see you later !")
-
-view_ = DuoButton(author, btn1, btn2).view()
-
-"""
-3 Simple Responsive Buttons
-"""
-
-btn1 = SButton(label="username", response=f"{ctx.author} ")
-btn2 = SButton(label="Bye", response="")
-btn3 = SButton(label="Bye", response="")
-
-"""
-Similarly You can Do the same to make 4 and 5 Buttons
-"""
-
-"""
-more coming ...
+Examples will be published soon ...
 
 """
