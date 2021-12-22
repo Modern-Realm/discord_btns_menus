58ab68ee44b1d6e04ac80b0b1929fc180a3c6f93
skrphenix <76821526+skrphenix@users.noreply.github.com>
2021-12-22 22:27:45 +0530

    0.1.7

---
+++ package/btns_menus/__init__.py
@@ -0,0 +1,10 @@
+"""
+Name: pycord-btns-menus
+Version: 0.1.7
+Summary: A responsive package for Buttons, DropMenus and Combinations
+Home-page: https://github.com/skrphenix/pycord_btns_menus
+Author: P. Sai Keerthan Reddy
+Author-email: saikeerthan.keerthan.9@gmail.com
+License: MIT
+Location: c:/users/sai keerthan/appdata/local/programs/python/python39/lib/site-packages
+"""
