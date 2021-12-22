2dc46f34d48773cab1af5fe6cebeeb220bcff354
skrphenix <76821526+skrphenix@users.noreply.github.com>
2021-12-22 21:34:33 +0530

    0.1.7

---
+++ setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycord-btns-menus",
+    version="0.1.7",
+    author="P. Sai Keerthan Reddy",
+    author_email="saikeerthan.keerthan.9@gmail.com",
+    description="A responsive package for Buttons, DropMenus and Combinations.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/skrphenix/pycord_btns_menus",
+    project_urls={
+        "Documentation": "https://skrphenix.github.io/pycord_btns_menus/",
+        "Bug Tracker": "https://github.com/skrphenix/pycord_btns_menus/issues",
+        "Examples": "https://github.com/skrphenix/pycord_btns_menus/tree/main/Examples",
+        "Source": "https://github.com/skrphenix/pycord_btns_menus/tree/main/package/btns_menus",
+        "Support Server": "https://discord.gg/GVMWx5EaAN",
+    },
+    keywords=["pycord", "py-cord", "btns", "pycord buttons",
+              "pycord btns", "pycord btns menus", "py-cord btns menus"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",
+    package_dir={"": "package"},
+    packages=["btns_menus", "btns_menus.builds", "btns_menus.builds.base"],
+    include_package_data=True,
+    python_requires=">=3.6",
+    dependency_links=[
+        "https://github.com/Pycord-Development/pycord"
+    ],
+    install_requires=[
+        "py-cord @ git+https://github.com/Pycord-Development/pycord@c8c2b1e23cb610b4c5f84c48fb23f279f2f0d53f"
+    ]
+)
