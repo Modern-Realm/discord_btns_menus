6105ff82194321198e362ca9f85cbf716126b82e
skrphenix <76821526+skrphenix@users.noreply.github.com>
2022-01-02 23:47:18 +0530

    0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pycord-btns-menus",
-    version="0.2.0",
+    version="0.2.1",
     author="P. Sai Keerthan Reddy",
     author_email="saikeerthan.keerthan.9@gmail.com",
     description="A responsive package for Buttons, DropMenus, Combinations and Paginator",
