c3765aa4eb37e96351647804dc486289c2063319
skrphenix <76821526+skrphenix@users.noreply.github.com>
2022-02-13 21:24:50 +0530

    changed verify_ parem to verify

--- Examples/buttons.py
+++ Examples/buttons.py
@@ -144,7 +144,7 @@ async def updating_button(ctx):
 async def reaction_role_button(ctx):
     user = ctx.author
     role = utils.get(ctx.guild.roles, id=ROLE_ID)  # Make sure to mention the ROLE_ID
-    reaction_btn = SButton(label="Verify", response="Verified !", ephemeral=True, verify_=False)
+    reaction_btn = SButton(label="Verify", response="Verified !", ephemeral=True, verify=False)
 
     async def give_role(button: SButton):
         button_user = button.interaction.user
