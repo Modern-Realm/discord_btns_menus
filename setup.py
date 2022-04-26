0d9c0b3d0999207df1acf2e4e6e6df3cb20d0104
skrphenix <76821526+skrphenix@users.noreply.github.com>
2022-04-26 19:07:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,6 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-extras_require = {
-    "discordpy": ["discord.py@ git+https://github.com/Rapptz/discord.py"],
-    "nextcord": ["nextcord@ git+https://github.com/nextcord/nextcord"],
-    "pycord": ["py-cord@ git+https://github.com/Pycord-Development/pycord"]
-}
-
 setuptools.setup(
     name="discord-btns-menus",
     version="0.2.3",
@@ -44,6 +38,5 @@ setuptools.setup(
     package_dir={"": "package"},
     packages=["btns_menus", "btns_menus.builds"],
     include_package_data=True,
-    python_requires=">=3.8",
-    extras_require=extras_require
+    python_requires=">=3.8"
 )
