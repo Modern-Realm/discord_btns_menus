246f29c3496da854101578df84a5ea93716f8ac3
skrphenix <76821526+skrphenix@users.noreply.github.com>
2022-04-26 19:02:54 +0530

    0.2.3

--- package/btns_menus/__init__.py
+++ package/btns_menus/__init__.py
@@ -1,10 +1,6 @@
 """
 Name: discord-btns-menus
 
-Version: 0.2.3
-
-Summary: A responsive package for Buttons, DropMenus and Combinations
-
 Home-page: https://github.com/Modern-Realm/discord_btns_menus
 
 Author: P. Sai Keerthan Reddy
@@ -13,3 +9,6 @@ Author-email: saikeerthan.keerthan.9@gmail.com
 
 License: MIT
 """
+
+__version__ = "0.2.3"
+__author__ = "P. Sai keerthan Reddy"
